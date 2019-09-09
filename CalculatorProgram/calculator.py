bfebbb172f22cfea77ce4c6eb0b401063c932cf1
rhhsu <50847645+rhhsu@users.noreply.github.com>
2019-09-09 12:23:29 -0400

    most recent: wrote function to return HDL result

--- CalculatorProgram/calculator.py
+++ CalculatorProgram/calculator.py
@@ -1,10 +1,21 @@
+def check_HDL(HDL_result):
+    if HDL_result >= 60:
+        return "Normal"
+    elif 40 <= HDL < 60:
+        return "Borderline low"
+    else:
+        return "low"
 def interface():
     print("My calculator program")
-    print("Options:")
-    print("9-Quit")
-    choice = input("Enter your choice")
-    if choice =='9':
-        return
+    keep_running = True
+    while keep_running:
+        print("My Program")
+        print("Options")
+        print("9-Quit")
+        choice = input("Enter your choice:")
+        if choice == '9':
+            keep_running = False
+    return
 
     if __name__ == '__main__':
         interface()
